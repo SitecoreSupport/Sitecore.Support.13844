5c42bde193009a4dbfc148cceb22eb43ea91ce2a
OlegBraginskyy <obr@sitecore.net>
2020-08-21 17:23:19 +0300

    Init

---
+++ src/Sitecore.Support.13844/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13844")]
+[assembly: AssemblyProduct("Sitecore.Support.13844")]
+[assembly: ComVisible(false)]
